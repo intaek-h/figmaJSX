dc6fa23ff5bd46572acc87388b04fdc983461552
Intaek H <ahiou2ahiou@gmail.com>
2022-07-01 00:08:32 +0900

    NEW: 가짜 줌 기능 추가

--- src/components/molecules/ToolPreset/index.js
+++ src/components/molecules/ToolPreset/index.js
@@ -1,14 +1,28 @@
-import ToolBox from "../../atoms/ToolBox";
+// import ToolBox from "../../atoms/ToolBox";
+import { lazy, Suspense } from "react";
+import MockToolBox from "../../atoms/ToolBox/MockToolBox";
 import styles from "./ToolPreset.module.scss";
+
+const ToolBox = lazy(() => import("../../atoms/ToolBox"));
 
 function ToolPreset() {
   return (
     <div className={styles.preset}>
-      <ToolBox tool="rectangle" />
-      <ToolBox tool="ellipse" />
-      <ToolBox tool="line" />
-      <ToolBox tool="selector" />
-      <ToolBox tool="text" />
+      <Suspense fallback={<MockToolBox />}>
+        <ToolBox tool="rectangle" />
+      </Suspense>
+      <Suspense fallback={<MockToolBox />}>
+        <ToolBox tool="ellipse" />
+      </Suspense>
+      <Suspense fallback={<MockToolBox />}>
+        <ToolBox tool="line" />
+      </Suspense>
+      <Suspense fallback={<MockToolBox />}>
+        <ToolBox tool="selector" />
+      </Suspense>
+      <Suspense fallback={<MockToolBox />}>
+        <ToolBox tool="text" />
+      </Suspense>
     </div>
   );
 }
